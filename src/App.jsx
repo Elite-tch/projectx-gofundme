4a3a48e535f81b480a1c4cd313c36aa64b3a210b
umezinwa vincent <umezvincent20@gmail.com>
2024-10-31 00:28:27 +0100

    made the Metrofund button connect with the create a page section

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Donate from "./pages/Donate";
-import CategoryPage from "./pages/CategoryPage";
-import Education from "./pages/Education";
+
 import CreateInitiative from "./pages/Create";
 
 function App() {
@@ -13,8 +12,7 @@ function App() {
 				<Routes>
 					<Route index path='/' element={<Home />} />
 					<Route index path='/Donate' element={<Donate />} />
-					<Route path='/category/:category' element={<CategoryPage />} />
-					<Route index path='category/education' element={<Education />} />
+				
 					<Route index path='/donate' element={<Donate />} />
 					<Route index path='/create' element={<CreateInitiative />} />
 				</Routes>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoCloseCircleOutline } from "react-icons/io5";
 import logo2 from "../assets/logo2.png";
-
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
@@ -13,57 +12,46 @@ const Navbar = () => {
 	const navLinksRight = [
 		{ title: "About", to: "#" },
 		{ title: "Help Center", to: "#" },
-		{ title: "Start a GoFundMe", to: "#" },
+		{ title: "Start a MetroFund", to: "/create" },
 	];
 
 	const [isOpen, setIsOpen] = useState(false);
 
-	// Function to handle the Start a GoFundMe click
-	const handleBookNowClick = (e) => {
-		e.preventDefault();
-		alert("Booking functionality coming soon!");
-	};
-
 	return (
 		<div className='w-full mx-auto px-5 lg:px-10 bg-white shadow-2xl'>
 			<div className='flex justify-between items-center py-2 lg:py-2'>
-				{/* Desktop Links for the  Left Side */}
+				{/* Desktop Links for the Left Side */}
 				<div className='hidden lg:flex space-x-5'>
 					{navLinksLeft.map((link, index) => (
-						<a
+						<Link
 							key={index}
-							href={link.to === "#" ? handleBookNowClick : link.to}
+							to={link.to === "#" ? "" : link.to}
 							className='text-sm cursor-pointer font-medium text-gray-700 hover:text-gray-900 transition duration-300'
 						>
 							{link.title}
-						</a>
+						</Link>
 					))}
 				</div>
 
-				{/* Logo which is placed in the  Center for desktop view */}
+				{/* Logo which is placed in the Center for desktop view */}
 				<Link to='/'>
-					<img src={logo2} alt='Logo' className='w-32 md:w-44 ' />
+					<img src={logo2} alt='Logo' className='w-32 md:w-44' />
 				</Link>
 
-				{/* Desktop Links for the Right hand Side */}
-				<div className='hidden lg:flex space-x-5 '>
+				{/* Desktop Links for the Right Side */}
+				<div className='hidden lg:flex space-x-5'>
 					{navLinksRight.map((link, index) => (
-						<a
+						<Link
 							key={index}
-							href={link.to === "#" ? undefined : link.to}
-							onClick={
-								link.title === "Start a GoFundMe"
-									? handleBookNowClick
-									: undefined
-							}
+							to={link.to === "#" ? "" : link.to}
 							className={`text-sm cursor-pointer font-medium hover:text-gray-900 transition duration-300 ${
-								link.title === "Start a GoFundMe"
+								link.title === "Start a MetroFund"
 									? "text-white bg-purple-700 px-4 py-2 rounded-full"
 									: "text-gray-700"
 							}`}
 						>
 							{link.title}
-						</a>
+						</Link>
 					))}
 				</div>
 
@@ -79,46 +67,42 @@ const Navbar = () => {
 				</div>
 			</div>
 
-			{/* Mobile slidin Menu */}
+			{/* Mobile Sliding Menu */}
 			<div
 				className={`fixed top-0 right-0 h-full bg-white transition-transform duration-300 ease-in-out w-4/5 max-w-xs shadow-lg z-50 px-2 ${
-					isOpen ? "transform translate-x-0" : "transform translate-x-full"
+					isOpen ? "translate-x-0" : "translate-x-full"
 				}`}
 			>
 				<div className='flex justify-between items-center p-5'>
-					{/* Logo since we dont have any logo for now */}
+					{/* Logo */}
 					<Link to='/' onClick={() => setIsOpen(false)}>
 						<img src={logo2} alt='Logo' className='h-10' />
 					</Link>
 
-					{/* Close X Button */}
+					{/* Close Button */}
 					<button onClick={() => setIsOpen(false)}>
 						<IoCloseCircleOutline size='1.5rem' />
 					</button>
 				</div>
 				<div className='mt-4 space-y-4'>
 					{[...navLinksLeft, ...navLinksRight].map((link, index) => (
-						<a
+						<Link
 							key={index}
-							href={link.to === "#" ? undefined : link.to}
-							onClick={() => {
-								setIsOpen(false);
-								if (link.title === "Start a GoFundMe") handleBookNowClick();
-							}}
+							to={link.to === "#" ? "" : link.to}
+							onClick={() => setIsOpen(false)}
 							className={`block text-lg font-medium px-4 py-2 transition duration-200 rounded-lg ${
-								link.title === "Start a GoFundMe"
+								link.title === "Start a MetroFund"
 									? "text-white bg-purple-700 rounded-full"
-									
 									: "text-gray-700 bg-gray-200 hover:bg-gray-300"
 							}`}
 						>
 							{link.title}
-						</a>
+						</Link>
 					))}
 				</div>
 			</div>
 
-			{/*Black Overlay for mobile menu */}
+			{/* Black Overlay for Mobile Menu */}
 			{isOpen && (
 				<div
 					className='fixed inset-0 bg-black opacity-50 z-40'
